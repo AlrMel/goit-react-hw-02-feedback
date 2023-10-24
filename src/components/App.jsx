7047b762a9d5b038e5e25727b1f3055a3fca0b5e
ArlMel <alexandrm1233@gmail.com>
2023-10-24 13:37:30 +0300

    xz02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,20 +8,26 @@ export class App extends Component {
     bad: 0,
   };
 
-  hendelIncrement = () => {};
+  hendelIncrement = (OIJLJLJ) => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good +1,
+      }
+    })
+  };
 
   render() {
     return (
       <div>
         <h1>Cafe Expresso</h1>
         <h2>Please leave feedback</h2>
-        <button>Good</button>
+        <button onClick={() => this.hendelIncrement(this.state.good)}>Good</button>
         <button>neutral</button>
         <button>bad</button>
         <h2>Statistics</h2>
-        <span>0</span>
-        <span>0</span>
-        <span>0</span>
+        <span>{this.state.good}</span>
+        <span>{this.state.neutral}</span>
+        <span>{this.state.bad}</span>
 
         {/* <Counter /> */}
       </div>
